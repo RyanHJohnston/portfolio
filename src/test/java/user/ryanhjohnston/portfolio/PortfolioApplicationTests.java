207c31f3a18cb3006497647778254362f8556d38
ryanhjohnston <ryanhjohnstoncollege@gmail.com>
2023-11-09 14:16:11 -0600

    first commit for website

---
+++ src/test/java/user/ryanhjohnston/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package user.ryanhjohnston.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
